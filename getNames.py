7f12c3a9d61d981747f2a852a961eedc441274ba
Demonic-code <115573131+Demonic-code@users.noreply.github.com>
2024-04-14 12:11:03 +0200

    Update getNames.py
    Made so it stays open.

--- getNames.py
+++ getNames.py
@@ -1,7 +1,7 @@
 import requests
 import time
 
-def getNames(username):
+def getName(username):
     response = requests.get(f"https://api.mojang.com/users/profiles/minecraft/{username}")
     return response.status_code != 200
 
@@ -13,18 +13,16 @@ with open("nameList.txt") as fp:
         name = line.strip()
         if name.isalpha():
             if len(set(name)) <= 25 and len(set(name)) >= 3: 
-                if getNames(name):
+                if getName(name):
                     nameFile = open('nameFound.txt', 'a')
                     nameFile.write(f"{name}\n")
                     nameFile.close()
                     
-                    
                     print("Available name found:", name)
                     count += 1   
         
-        time.sleep(1.5)
+        time.sleep(1.7)
         
-    print("We found", count, "Names")             
-        
-     
-     
+    print("We found", count, "Names")
+
+input("Press enter to close")
